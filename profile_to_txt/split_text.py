e22fdbc2b1ae8097e6c3dad193e2d2cb646a124e
Alexander Goedicke <alexander.goedicke@cassini.de>
2020-02-13 16:14:20 +0100

    analyse several profiles at once

---
+++ profile_to_txt/split_text.py
@@ -0,0 +1,44 @@
+# TODO:
+#   - split at spaces, where possible, otherwise just split.
+def split_text(txt, length):
+    inp = txt
+    out = []
+
+    while len(inp)>length:
+        out.append(inp[:length])
+        inp = inp[length:]
+
+    if len(inp)>0:
+        out.append(inp)
+
+    return out
+
+
+def create_inp_json(text, ata_max_len = 5000, language='de'):
+    text_list = split_text(text, ata_max_len)
+#    print(text_list)
+    docs = [
+            {'language': language, 'id': i+1, 'text': t}
+            for (i,t) in enumerate(text_list)
+            ]
+    return {'documents': docs }
+
+
+def join_inp_json(json_list):
+    ''' Join input jsons, using the language of the first document. '''
+    i = 0 # counter for the documents
+    docs_out = []
+    language = json_list[0]['documents'][0]['language']
+    for j in json_list:
+        docs = j['documents']
+        for d in docs:
+            i = i + 1
+            docs_out.append({'language': language, 'id': i, 'text': d['text']})
+    return {'documents': docs_out}
+
+
+if __name__ == '__main__':
+    print(split_text('This is my text.', 5))
+    print(create_inp_json('This is my text.', 5))
+    l = [create_inp_json('This is my first text.', 5), create_inp_json('This is my second text.', 5)]
+    print(join_inp_json(l))
--- profile_to_txt/test.py
+++ profile_to_txt/test.py
@@ -1,40 +1,62 @@
 from reader import *
+from split_text import *
 import requests
 import json
 
-headers = {"Ocp-Apim-Subscription-Key": "222a089a07714a65bcd309f9a2434ffa",
+# headers for text analytics
+headers = {"Ocp-Apim-Subscription-Key": "dcb0d307225f4a70a5bc77be156bc529",
     "Content-Type": "application/json",
     "Accept": "application/json",
     "cache-control": "no-cache",
     "Postman-Token": "45a3c16d-12a7-445c-8ace-545d219cbff7"
     }
 
-with open("input.json", "r") as f:
-    data = json.loads(f.read())
 
-url = "https://profilekeywords.cognitiveservices.azure.com/text/analytics/v2.1/keyPhrases"
+def analyse_json_list(json_list):
+#   with open("input.json", "r") as f:
+#       data = json.loads(f.read())
 
-eq = requests.Request('POST', url, headers = headers, json = data)
-prepared = eq.prepare()
+    data = json_list
 
-def pretty_print_POST(req):
-    """
-    At this point it is completely built and ready
-    to be fired; it is "prepared".
+    url = "https://profilekeywords.cognitiveservices.azure.com/text/analytics/v2.1/keyPhrases"
 
-    However pay attention at the formatting used in
-    this function because it is programmed to be pretty
-    printed and may differ from the actual request.
-    """
-    print('{}\n{}\r\n{}\r\n\r\n{}'.format(
-        '-----------START-----------',
-        req.method + ' ' + req.url,
-        '\r\n'.join('{}: {}'.format(k, v) for k, v in req.headers.items()),
-        req.body,
-    ))
+    eq = requests.Request('POST', url, headers = headers, json = data)
+    prepared = eq.prepare()
 
-pretty_print_POST(prepared)
+#    def pretty_print_POST(req):
+#        """
+#        At this point it is completely built and ready
+#        to be fired; it is "prepared".
+#    
+#        However pay attention at the formatting used in
+#        this function because it is programmed to be pretty
+#        printed and may differ from the actual request.
+#        """
+#        print('{}\n{}\r\n{}\r\n\r\n{}'.format(
+#            '-----------START-----------',
+#            req.method + ' ' + req.url,
+#            '\r\n'.join('{}: {}'.format(k, v) for k, v in req.headers.items()),
+#            req.body,
+#        ))
+#
+#    pretty_print_POST(prepared)
 
-r = requests.post(url, headers=headers, json = data)
-print(r.status_code, r.reason)
-print(r.content.decode('utf8'))
+    r = requests.post(url, headers=headers, json = data)
+#    print(r.status_code, r.reason)
+    return r.content.decode('utf8')
+
+
+def generate_json_list(path_list):
+    out_list = [create_inp_json(read_docx(p)) for p in path_list]
+    return join_inp_json(out_list)
+
+
+if __name__ == '__main__':
+    profiles = [
+            '../agd_profil.docx',
+            '/Users/cassini/Cassini AG/CAS Mitarbeiterprofile - Dokumente/A/Achterberg, Manuel/Manuel_Achterberg_Langprofil.docx',
+            '/Users/cassini/Cassini AG/CAS Mitarbeiterprofile - Dokumente/K/Kalisch, Dominik/Kalisch_Dominik_Profil_lang.docx'
+            ]
+    json_list = generate_json_list(profiles)
+    print(analyse_json_list(json_list))
+
